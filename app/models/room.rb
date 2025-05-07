43daa000d28e2bd08d1b7a17f3839db488a3c90f
utage-yuta <yuta.nakamura.1381@gmail.com>
2025-05-07 13:19:10 +0900

    各モデルのテスト

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,6 @@ class Room < ApplicationRecord
   has_many :users,through: :room_users
   has_many :messages, dependent: :destroy
   validates :name, presence: true
+
+  validates :name, presence: true
 end
